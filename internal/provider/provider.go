e46349f23471849a8030f3f131ca11a1e9d379dc
Ben Meier <ben.meier@humanitec.com>
2025-09-22 09:58:04 +0100

    fix: loading hctl config file

--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -11,6 +11,7 @@ import (
 	"path/filepath"
 	"time"
 
+	"github.com/hashicorp/terraform-plugin-log/tflog"
 	"gopkg.in/yaml.v3"
 
 	canyoncp "terraform-provider-humanitec-v2/internal/clients/canyon-cp"
@@ -132,15 +133,16 @@ func readConfigFile(path string) (Config, error) {
 }
 
 func getConfigFilePath() (string, error) {
-	homeDir, err := os.UserHomeDir()
-	if err != nil {
-		return "", fmt.Errorf("failed to get user home directory: %w", err)
+	homeDirPath, _ := os.UserHomeDir()
+	homeDirPath = filepath.Join(homeDirPath, ".config", "hctl", "config.yaml")
+	cfgDirPath, _ := os.UserConfigDir()
+	cfgDirPath = filepath.Join(cfgDirPath, "hctl", "config.yaml")
+	if _, err := os.Stat(cfgDirPath); err == nil {
+		return cfgDirPath, nil
+	} else if _, err := os.Stat(homeDirPath); err == nil {
+		return homeDirPath, nil
 	}
-	configDir, err := os.UserConfigDir()
-	if err != nil {
-		configDir = filepath.Join(homeDir, ".config")
-	}
-	return filepath.Join(configDir, "hctl", "config.yaml"), nil
+	return "", fmt.Errorf("failed to find hctl config file path: neither %s nor %s exists", cfgDirPath, homeDirPath)
 }
 
 func (p *HumanitecProvider) Configure(ctx context.Context, req provider.ConfigureRequest, resp *provider.ConfigureResponse) {
@@ -161,11 +163,13 @@ func (p *HumanitecProvider) Configure(ctx context.Context, req provider.Configur
 		if cfg, err := readConfigFile(p); err != nil {
 			resp.Diagnostics.AddError(HUM_PROVIDER_ERR, fmt.Sprintf("Failed to read config file '%s': %s", p, err))
 		} else {
-			if cfg.ApiUrl != "" {
+			if apiUrl == "" && cfg.ApiUrl != "" {
 				apiUrl = cfg.ApiUrl
-			} else if cfg.DefaultOrg != "" {
+			}
+			if orgId == "" && cfg.DefaultOrg != "" {
 				orgId = cfg.DefaultOrg
-			} else if cfg.Token != "" {
+			}
+			if authToken == "" && cfg.Token != "" {
 				authToken = cfg.Token
 			}
 		}
@@ -173,12 +177,15 @@ func (p *HumanitecProvider) Configure(ctx context.Context, req provider.Configur
 
 	// SECOND - we fall back to environment variables
 	if v := os.Getenv(HUM_API_URL_ENV_VAR); apiUrl == "" && v != "" {
+		tflog.Debug(ctx, "using platform-orchestrator api url from environment variable")
 		apiUrl = v
 	}
 	if v := os.Getenv(HUM_ORG_ID_ENV_VAR); orgId == "" && v != "" {
+		tflog.Debug(ctx, "using platform-orchestrator org id from environment variable")
 		orgId = v
 	}
 	if v := os.Getenv(HUM_AUTH_TOKEN_ENV_VAR); authToken == "" && v != "" {
+		tflog.Debug(ctx, "using platform-orchestrator auth token from environment variable")
 		authToken = v
 	}
 
@@ -191,11 +198,16 @@ func (p *HumanitecProvider) Configure(ctx context.Context, req provider.Configur
 				resp.Diagnostics.AddError(HUM_PROVIDER_ERR, fmt.Sprintf("Failed to read config file '%s': %s", p, err))
 			}
 		} else {
-			if cfg.ApiUrl != "" {
+			if apiUrl == "" && cfg.ApiUrl != "" {
+				tflog.Debug(ctx, "using platform-orchestrator api url from hctl config file")
 				apiUrl = cfg.ApiUrl
-			} else if cfg.DefaultOrg != "" {
+			}
+			if orgId == "" && cfg.DefaultOrg != "" {
+				tflog.Debug(ctx, "using platform-orchestrator org id from hctl config file")
 				orgId = cfg.DefaultOrg
-			} else if cfg.Token != "" {
+			}
+			if authToken == "" && cfg.Token != "" {
+				tflog.Debug(ctx, "using platform-orchestrator auth token from hctl config file")
 				authToken = cfg.Token
 			}
 		}

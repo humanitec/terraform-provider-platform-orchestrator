4bbaf48327c23b560cca30466865122c230e5f17
Ben Meier <ben.meier@humanitec.com>
2025-10-02 08:00:51 +0100

    chore: first common runner check

--- internal/provider/kubernetes_agent_runner_data_source.go
+++ internal/provider/kubernetes_agent_runner_data_source.go
@@ -2,8 +2,7 @@ package provider
 
 import (
 	"context"
-	"fmt"
-	"net/http"
+	"errors"
 	"regexp"
 
 	canyoncp "terraform-provider-humanitec-v2/internal/clients/canyon-cp"
@@ -16,164 +15,99 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/types"
 )
 
-// Ensure provider defined types fully satisfy framework interfaces.
-var _ datasource.DataSource = &KubernetesAgentRunnerDataSource{}
-
 func NewKubernetesAgentRunnerDataSource() datasource.DataSource {
-	return &KubernetesAgentRunnerDataSource{}
-}
+	return &commonRunnerDataSource{
+		SubType: "kubernetes_agent_runner",
+		SchemaDef: schema.Schema{
+			// This description is used by the documentation generator and the language server.
+			MarkdownDescription: "Kubernetes Agent Runner data source",
 
-// KubernetesAgentRunnerDataSource defines the data source implementation.
-type KubernetesAgentRunnerDataSource struct {
-	cpClient canyoncp.ClientWithResponsesInterface
-	orgId    string
-}
-
-// KubernetesAgentRunnerDataSourceModel describes the data source data model.
-type KubernetesAgentRunnerDataSourceModel struct {
-	Id                        types.String `tfsdk:"id"`
-	Description               types.String `tfsdk:"description"`
-	RunnerConfiguration       types.Object `tfsdk:"runner_configuration"`
-	StateStorageConfiguration types.Object `tfsdk:"state_storage_configuration"`
-}
-
-func (d *KubernetesAgentRunnerDataSource) Metadata(ctx context.Context, req datasource.MetadataRequest, resp *datasource.MetadataResponse) {
-	resp.TypeName = req.ProviderTypeName + "_kubernetes_agent_runner"
-}
-
-func (d *KubernetesAgentRunnerDataSource) Schema(ctx context.Context, req datasource.SchemaRequest, resp *datasource.SchemaResponse) {
-	resp.Schema = schema.Schema{
-		// This description is used by the documentation generator and the language server.
-		MarkdownDescription: "Kubernetes Agent Runner data source",
-
-		Attributes: map[string]schema.Attribute{
-			"id": schema.StringAttribute{
-				MarkdownDescription: "Kubernetes Agent Runner ID",
-				Required:            true,
-				Validators: []validator.String{
-					stringvalidator.RegexMatches(
-						regexp.MustCompile(`^[a-z](?:-?[a-z0-9]+)+$`),
-						"must start with a lowercase letter, can contain lowercase letters, numbers, and hyphens and can not be empty.",
-					),
-				},
-			},
-			"description": schema.StringAttribute{
-				MarkdownDescription: "Kubernetes Agent Runner description",
-				Computed:            true,
-			},
-			"runner_configuration": schema.SingleNestedAttribute{
-				MarkdownDescription: "The configuration of the Kubernetes Agent Runner",
-				Computed:            true,
-				Attributes: map[string]schema.Attribute{
-					"key": schema.StringAttribute{
-						MarkdownDescription: "The public ed25519 key in PEM format used to identify the caller identity",
-						Computed:            true,
+			Attributes: map[string]schema.Attribute{
+				"id": schema.StringAttribute{
+					MarkdownDescription: "Kubernetes Agent Runner ID",
+					Required:            true,
+					Validators: []validator.String{
+						stringvalidator.RegexMatches(
+							regexp.MustCompile(`^[a-z](?:-?[a-z0-9]+)+$`),
+							"must start with a lowercase letter, can contain lowercase letters, numbers, and hyphens and can not be empty.",
+						),
 					},
-					"job": schema.SingleNestedAttribute{
-						MarkdownDescription: "The job configuration for the Kubernetes Job triggered by the Kubernetes Agent Runner",
-						Computed:            true,
-						Attributes: map[string]schema.Attribute{
-							"namespace": schema.StringAttribute{
-								MarkdownDescription: "The namespace for the Kubernetes Runner job",
-								Computed:            true,
-							},
-							"service_account": schema.StringAttribute{
-								MarkdownDescription: "The service account for the Kubernetes Runner job",
-								Computed:            true,
-							},
-							"pod_template": schema.StringAttribute{
-								MarkdownDescription: "JSON encoded pod template for the Kubernetes Runner job",
-								Computed:            true,
-								CustomType:          jsontypes.NormalizedType{},
+				},
+				"description": schema.StringAttribute{
+					MarkdownDescription: "Kubernetes Agent Runner description",
+					Computed:            true,
+				},
+				"runner_configuration": schema.SingleNestedAttribute{
+					MarkdownDescription: "The configuration of the Kubernetes Agent Runner",
+					Computed:            true,
+					Attributes: map[string]schema.Attribute{
+						"key": schema.StringAttribute{
+							MarkdownDescription: "The public ed25519 key in PEM format used to identify the caller identity",
+							Computed:            true,
+						},
+						"job": schema.SingleNestedAttribute{
+							MarkdownDescription: "The job configuration for the Kubernetes Job triggered by the Kubernetes Agent Runner",
+							Computed:            true,
+							Attributes: map[string]schema.Attribute{
+								"namespace": schema.StringAttribute{
+									MarkdownDescription: "The namespace for the Kubernetes Runner job",
+									Computed:            true,
+								},
+								"service_account": schema.StringAttribute{
+									MarkdownDescription: "The service account for the Kubernetes Runner job",
+									Computed:            true,
+								},
+								"pod_template": schema.StringAttribute{
+									MarkdownDescription: "JSON encoded pod template for the Kubernetes Runner job",
+									Computed:            true,
+									CustomType:          jsontypes.NormalizedType{},
+								},
 							},
 						},
 					},
 				},
-			},
-			"state_storage_configuration": schema.SingleNestedAttribute{
-				MarkdownDescription: "The state storage configuration for the Kubernetes Agent Runner",
-				Computed:            true,
-				Attributes: map[string]schema.Attribute{
-					"type": schema.StringAttribute{
-						MarkdownDescription: "The type of state storage configuration for the Kubernetes Agent Runner",
-						Computed:            true,
-					},
-					"kubernetes_configuration": schema.SingleNestedAttribute{
-						MarkdownDescription: "The Kubernetes state storage configuration for the Kubernetes Agent Runner",
-						Computed:            true,
-						Attributes: map[string]schema.Attribute{
-							"namespace": schema.StringAttribute{
-								MarkdownDescription: "The namespace for the Kubernetes state storage configuration",
-								Computed:            true,
+				"state_storage_configuration": schema.SingleNestedAttribute{
+					MarkdownDescription: "The state storage configuration for the Kubernetes Agent Runner",
+					Computed:            true,
+					Attributes: map[string]schema.Attribute{
+						"type": schema.StringAttribute{
+							MarkdownDescription: "The type of state storage configuration for the Kubernetes Agent Runner",
+							Computed:            true,
+						},
+						"kubernetes_configuration": schema.SingleNestedAttribute{
+							MarkdownDescription: "The Kubernetes state storage configuration for the Kubernetes Agent Runner",
+							Computed:            true,
+							Attributes: map[string]schema.Attribute{
+								"namespace": schema.StringAttribute{
+									MarkdownDescription: "The namespace for the Kubernetes state storage configuration",
+									Computed:            true,
+								},
 							},
 						},
 					},
 				},
 			},
 		},
+		ReadApiResponseIntoModel: func(item canyoncp.Runner, model commonRunnerModel) (commonRunnerModel, error) {
+			k8sAgentRunnerConfiguration, _ := item.RunnerConfiguration.AsK8sAgentRunnerConfiguration()
+			k8sStateStorageConfiguration, _ := item.StateStorageConfiguration.AsK8sStorageConfiguration()
+
+			runnerConfigurationModel, err := parseKubernetesAgentRunnerConfigurationResponse(context.Background(), k8sAgentRunnerConfiguration)
+			if err != nil {
+				return commonRunnerModel{}, err
+			}
+
+			stateStorageConfigurationModel := parseStateStorageConfigurationResponse(context.Background(), k8sStateStorageConfiguration)
+			if stateStorageConfigurationModel == nil {
+				return commonRunnerModel{}, errors.New("failed to parse state storage configuration")
+			}
+
+			return commonRunnerModel{
+				Id:                        types.StringValue(item.Id),
+				Description:               types.StringPointerValue(item.Description),
+				StateStorageConfiguration: *stateStorageConfigurationModel,
+				RunnerConfiguration:       runnerConfigurationModel,
+			}, nil
+		},
 	}
-}
-
-func (d *KubernetesAgentRunnerDataSource) Configure(ctx context.Context, req datasource.ConfigureRequest, resp *datasource.ConfigureResponse) {
-	// Prevent panic if the provider has not been configured.
-	if req.ProviderData == nil {
-		return
-	}
-
-	providerData, ok := req.ProviderData.(*HumanitecProviderData)
-	if !ok {
-		resp.Diagnostics.AddError(
-			HUM_PROVIDER_ERR,
-			fmt.Sprintf("Expected *HumanitecProviderData, got: %T. Please report this issue to the provider developers.", req.ProviderData),
-		)
-		return
-	}
-
-	d.cpClient = providerData.CpClient
-	d.orgId = providerData.OrgId
-}
-
-func (d *KubernetesAgentRunnerDataSource) Read(ctx context.Context, req datasource.ReadRequest, resp *datasource.ReadResponse) {
-	var data KubernetesAgentRunnerDataSourceModel
-
-	// Read Terraform configuration data into the model
-	resp.Diagnostics.Append(req.Config.Get(ctx, &data)...)
-
-	if resp.Diagnostics.HasError() {
-		return
-	}
-
-	httpResp, err := d.cpClient.GetRunnerWithResponse(ctx, d.orgId, data.Id.ValueString())
-	if err != nil {
-		resp.Diagnostics.AddError(HUM_CLIENT_ERR, fmt.Sprintf("Unable to read kubernetes agent runner, got error: %s", err))
-		return
-	}
-
-	if httpResp.StatusCode() == http.StatusNotFound {
-		resp.Diagnostics.AddError(HUM_RESOURCE_NOT_FOUND_ERR, fmt.Sprintf("Kubernetes agent runner with ID %s not found in org %s", data.Id.ValueString(), d.orgId))
-		resp.State.RemoveResource(ctx)
-		return
-	}
-
-	if httpResp.StatusCode() != http.StatusOK {
-		resp.Diagnostics.AddError(HUM_API_ERR, fmt.Sprintf("Unable to read kubernetes agent runner, unexpected status code: %d, body: %s", httpResp.StatusCode(), httpResp.Body))
-		return
-	}
-
-	runner := httpResp.JSON200
-
-	data.Id = types.StringValue(runner.Id)
-	data.Description = types.StringPointerValue(runner.Description)
-
-	// Convert the runner to the data source model
-	if convertedData, err := toKubernetesAgentRunnerResourceModel(*runner); err != nil {
-		resp.Diagnostics.AddError(HUM_PROVIDER_ERR, fmt.Sprintf("Failed to convert API response to KubernetesAgentRunnerDataSourceModel: %s", err))
-		return
-	} else {
-		data.RunnerConfiguration = convertedData.RunnerConfiguration
-		data.StateStorageConfiguration = convertedData.StateStorageConfiguration
-	}
-
-	// Save data into Terraform state
-	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
 }
---
+++ internal/provider/runner_common.go
@@ -0,0 +1,105 @@
+package provider
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+
+	"github.com/hashicorp/terraform-plugin-framework/datasource"
+	"github.com/hashicorp/terraform-plugin-framework/datasource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+
+	canyoncp "terraform-provider-humanitec-v2/internal/clients/canyon-cp"
+)
+
+// Ensure provider defined types fully satisfy framework interfaces.
+var _ datasource.DataSourceWithConfigure = &commonRunnerDataSource{}
+
+type commonRunnerDataSource struct {
+	SubType                  string
+	SchemaDef                schema.Schema
+	ReadApiResponseIntoModel func(canyoncp.Runner, commonRunnerModel) (commonRunnerModel, error)
+
+	// params set during Configure()
+	cpClient canyoncp.ClientWithResponsesInterface
+	orgId    string
+}
+
+type commonRunnerModel struct {
+	Id                        types.String `tfsdk:"id"`
+	Description               types.String `tfsdk:"description"`
+	RunnerConfiguration       types.Object `tfsdk:"runner_configuration"`
+	StateStorageConfiguration types.Object `tfsdk:"state_storage_configuration"`
+}
+
+func (d *commonRunnerDataSource) Metadata(ctx context.Context, req datasource.MetadataRequest, resp *datasource.MetadataResponse) {
+	resp.TypeName = req.ProviderTypeName + "_" + d.SubType
+}
+
+func (d *commonRunnerDataSource) Schema(ctx context.Context, req datasource.SchemaRequest, resp *datasource.SchemaResponse) {
+	resp.Schema = d.SchemaDef
+}
+
+func (d *commonRunnerDataSource) Configure(ctx context.Context, req datasource.ConfigureRequest, resp *datasource.ConfigureResponse) {
+	// Prevent panic if the provider has not been configured.
+	if req.ProviderData == nil {
+		return
+	}
+
+	providerData, ok := req.ProviderData.(*HumanitecProviderData)
+	if !ok {
+		resp.Diagnostics.AddError(
+			HUM_PROVIDER_ERR,
+			fmt.Sprintf("Expected *HumanitecProviderData, got: %T. Please report this issue to the provider developers.", req.ProviderData),
+		)
+		return
+	}
+
+	d.cpClient = providerData.CpClient
+	d.orgId = providerData.OrgId
+}
+
+func (d *commonRunnerDataSource) Read(ctx context.Context, req datasource.ReadRequest, resp *datasource.ReadResponse) {
+	var data commonRunnerModel
+
+	// Read Terraform configuration data into the model
+	resp.Diagnostics.Append(req.Config.Get(ctx, &data)...)
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	httpResp, err := d.cpClient.GetRunnerWithResponse(ctx, d.orgId, data.Id.ValueString())
+	if err != nil {
+		resp.Diagnostics.AddError(HUM_CLIENT_ERR, fmt.Sprintf("Unable to read kubernetes eks runner, got error: %s", err))
+		return
+	}
+
+	if httpResp.StatusCode() == http.StatusNotFound {
+		resp.Diagnostics.AddError(HUM_RESOURCE_NOT_FOUND_ERR, fmt.Sprintf("Kubernetes eks runner with ID %s not found in org %s", data.Id.ValueString(), d.orgId))
+		resp.State.RemoveResource(ctx)
+		return
+	}
+
+	if httpResp.StatusCode() != http.StatusOK {
+		resp.Diagnostics.AddError(HUM_API_ERR, fmt.Sprintf("Unable to read kubernetes eks runner, unexpected status code: %d, body: %s", httpResp.StatusCode(), httpResp.Body))
+		return
+	}
+
+	runner := httpResp.JSON200
+
+	data.Id = types.StringValue(runner.Id)
+	data.Description = types.StringPointerValue(runner.Description)
+
+	// Convert the runner to the data source model
+	if convertedData, err := d.ReadApiResponseIntoModel(*runner, data); err != nil {
+		resp.Diagnostics.AddError(HUM_PROVIDER_ERR, fmt.Sprintf("Failed to convert API response to KubernetesEksRunnerDataSourceModel: %s", err))
+		return
+	} else {
+		data.RunnerConfiguration = convertedData.RunnerConfiguration
+		data.StateStorageConfiguration = convertedData.StateStorageConfiguration
+	}
+
+	// Save data into Terraform state
+	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
+}

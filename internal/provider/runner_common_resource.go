d494680aec311f7c509a01f1852906373f8cd661
Ben Meier <1651305+astromechza@users.noreply.github.com>
2025-10-02 11:03:25 +0100

    chore: use status code constants
    Co-authored-by: Anton Korshikov <chado@chado.by>

--- internal/provider/runner_common_resource.go
+++ internal/provider/runner_common_resource.go
@@ -155,7 +155,7 @@ func (r *commonRunnerResource) Read(ctx context.Context, req resource.ReadReques
 		return
 	}
 
-	if httpResp.StatusCode() != 200 {
+	if httpResp.StatusCode() != http.StatusOK {
 		resp.Diagnostics.AddError(HUM_API_ERR, fmt.Sprintf("Unable to read runner, unexpected status code: %d, body: %s", httpResp.StatusCode(), httpResp.Body))
 		return
 	}

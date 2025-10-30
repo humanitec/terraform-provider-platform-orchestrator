bd805ae1a6da6b79d809b6c1c38153c9011c6cfd
Ben Meier <ben.meier@humanitec.com>
2025-10-30 13:08:24 +0000

    feat: added a terraform resource for deployments

--- go.mod
+++ go.mod
@@ -19,6 +19,7 @@ require (
 )
 
 require (
+	filippo.io/age v1.2.1 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 )
---
+++ internal/provider/deployment_resource.go
@@ -0,0 +1,303 @@
+package provider
+
+import (
+	"context"
+	"encoding/base64"
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+	"regexp"
+	"strings"
+	"time"
+
+	"filippo.io/age"
+	"github.com/google/uuid"
+	"github.com/hashicorp/terraform-plugin-framework-timeouts/resource/timeouts"
+	"github.com/hashicorp/terraform-plugin-framework-validators/stringvalidator"
+	"github.com/hashicorp/terraform-plugin-framework/diag"
+	"github.com/hashicorp/terraform-plugin-framework/resource"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/boolplanmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+
+	canyondp "terraform-provider-humanitec-v2/internal/clients/canyon-dp"
+	"terraform-provider-humanitec-v2/internal/ref"
+)
+
+var _ resource.Resource = &DeploymentResource{}
+var _ resource.ResourceWithConfigure = &DeploymentResource{}
+
+func NewDeploymentResource() resource.Resource {
+	return &DeploymentResource{}
+}
+
+type DeploymentResource struct {
+	dpClient canyondp.ClientWithResponsesInterface
+	orgId    string
+}
+
+type DeploymentResourceModel struct {
+	ProjectId     types.String   `tfsdk:"project_id"`
+	EnvId         types.String   `tfsdk:"env_id"`
+	Mode          types.String   `tfsdk:"mode"`
+	Id            types.String   `tfsdk:"id"`
+	CreatedAt     types.String   `tfsdk:"created_at"`
+	CompletedAt   types.String   `tfsdk:"completed_at"`
+	Status        types.String   `tfsdk:"status"`
+	StatusMessage types.String   `tfsdk:"status_message"`
+	RunnerId      types.String   `tfsdk:"runner_id"`
+	WaitFor       types.Bool     `tfsdk:"wait_for"`
+	Outputs       types.String   `tfsdk:"outputs"`
+	Timeouts      timeouts.Value `tfsdk:"timeouts"`
+}
+
+func (d *DeploymentResource) Metadata(ctx context.Context, request resource.MetadataRequest, response *resource.MetadataResponse) {
+	response.TypeName = request.ProviderTypeName + "_deployment"
+}
+
+func (d *DeploymentResource) Schema(ctx context.Context, request resource.SchemaRequest, response *resource.SchemaResponse) {
+	response.Schema = schema.Schema{
+		// This description is used by the documentation generator and the language server.
+		MarkdownDescription: "Deployment resource",
+
+		Attributes: map[string]schema.Attribute{
+			"project_id": schema.StringAttribute{
+				MarkdownDescription: "The project ID to deploy.",
+				Required:            true,
+				Validators: []validator.String{
+					stringvalidator.RegexMatches(
+						regexp.MustCompile(`^[a-z](?:-?[a-z0-9]+)+$`),
+						"must start with a lowercase letter, can contain lowercase letters, numbers, and hyphens and can not be empty.",
+					),
+				},
+				PlanModifiers: []planmodifier.String{
+					stringplanmodifier.RequiresReplace(),
+				},
+			},
+			"env_id": schema.StringAttribute{
+				MarkdownDescription: "The environment ID to deploy.",
+				Required:            true,
+				Validators: []validator.String{
+					stringvalidator.RegexMatches(
+						regexp.MustCompile(`^[a-z](?:-?[a-z0-9]+)+$`),
+						"must start with a lowercase letter, can contain lowercase letters, numbers, and hyphens and can not be empty.",
+					),
+				},
+				PlanModifiers: []planmodifier.String{
+					stringplanmodifier.RequiresReplace(),
+				},
+			},
+			"mode": schema.StringAttribute{
+				MarkdownDescription: "The mode of the deployment. 'deploy' (the default) or 'plan_only'.",
+				Computed:            true,
+				Optional:            true,
+				Validators: []validator.String{
+					stringvalidator.OneOf(string(canyondp.Deploy), string(canyondp.PlanOnly)),
+				},
+			},
+			"id": schema.StringAttribute{
+				MarkdownDescription: "The ID of the Deployment.",
+				Computed:            true,
+			},
+			"created_at": schema.StringAttribute{
+				MarkdownDescription: "The date and time when the deployment was created.",
+				Computed:            true,
+			},
+			"completed_at": schema.StringAttribute{
+				MarkdownDescription: "The date and time when the deployment was completed.",
+				Computed:            true,
+			},
+			"status": schema.StringAttribute{
+				MarkdownDescription: "The status of the deployment (succeeded, failed).",
+				Computed:            true,
+			},
+			"status_message": schema.StringAttribute{
+				MarkdownDescription: "An optional message associated with the status.",
+				Computed:            true,
+			},
+			"runner_id": schema.StringAttribute{
+				MarkdownDescription: "The ID of the runner used in this deployment.",
+				Computed:            true,
+			},
+			"wait_for": schema.BoolAttribute{
+				MarkdownDescription: "Whether to wait for the deployment to complete. Defaults to true. If false, the output will be empty.",
+				Computed:            true,
+				Optional:            true,
+				PlanModifiers: []planmodifier.Bool{
+					boolplanmodifier.UseStateForUnknown(),
+				},
+			},
+			"outputs": schema.StringAttribute{
+				MarkdownDescription: "The JSON encoded outputs of the deployment.",
+				Computed:            true,
+				Sensitive:           true,
+			},
+		},
+		Blocks: map[string]schema.Block{
+			"timeouts": timeouts.Block(ctx, timeouts.Opts{Delete: true}),
+		},
+	}
+}
+
+func (d *DeploymentResource) Configure(ctx context.Context, request resource.ConfigureRequest, response *resource.ConfigureResponse) {
+	if request.ProviderData == nil {
+		return
+	}
+
+	providerData, ok := request.ProviderData.(*HumanitecProviderData)
+	if !ok {
+		response.Diagnostics.AddError(
+			HUM_PROVIDER_ERR,
+			fmt.Sprintf("Expected *HumanitecProviderData, got: %T. Please report this issue to the provider developers.", request.ProviderData),
+		)
+		return
+	}
+
+	d.dpClient = providerData.DpClient
+	d.orgId = providerData.OrgId
+}
+
+func (d *DeploymentResource) doDeployment(ctx context.Context, data *DeploymentResourceModel, diags diag.Diagnostics) (outputsKey *age.X25519Identity) {
+	if data.Mode.IsNull() {
+		data.Mode = types.StringValue(string(canyondp.Deploy))
+	}
+	if data.WaitFor.IsNull() {
+		data.WaitFor = types.BoolValue(true)
+	}
+
+	outputsKey, _ = age.GenerateX25519Identity()
+	if r, err := d.dpClient.CreateDeploymentWithResponse(
+		ctx, d.orgId, &canyondp.CreateDeploymentParams{IdempotencyKey: ref.Ref(uuid.NewString())},
+		canyondp.DeploymentCreateBody{
+			ProjectId:                 data.ProjectId.ValueString(),
+			EnvId:                     data.EnvId.ValueString(),
+			Manifest:                  canyondp.DeploymentManifest{},
+			Mode:                      canyondp.DeploymentCreateBodyMode(data.Mode.ValueString()),
+			EncryptedOutputsRecipient: ref.Ref(outputsKey.Recipient().String()),
+		},
+	); err != nil {
+		diags.AddError(HUM_CLIENT_ERR, fmt.Sprintf("Unable to create deployment, got error: %s", err))
+		return
+	} else if r.StatusCode() != http.StatusCreated {
+		diags.AddError(HUM_API_ERR, fmt.Sprintf("Unable to create deployment, unexpected status code: %d, body: %s", r.StatusCode(), r.Body))
+		return
+	} else {
+		data.Id = types.StringValue(r.JSON201.Id.String())
+		data.CreatedAt = types.StringValue(r.JSON201.CreatedAt.Format(time.RFC3339))
+		data.CompletedAt = types.StringNull()
+		data.Outputs = types.StringNull()
+		data.Status = types.StringValue(string(r.JSON201.Status))
+		data.StatusMessage = types.StringValue(r.JSON201.StatusMessage)
+		data.RunnerId = types.StringValue(r.JSON201.RunnerId)
+	}
+	return outputsKey
+}
+
+func (d *DeploymentResource) waitForDeployment(ctx context.Context, data *DeploymentResourceModel, diags diag.Diagnostics, outputsKey *age.X25519Identity) {
+	for {
+		if r, err := d.dpClient.WaitForDeploymentCompleteWithResponse(ctx, d.orgId, uuid.MustParse(data.Id.ValueString()), &canyondp.WaitForDeploymentCompleteParams{}); err != nil {
+			if errors.Is(err, context.DeadlineExceeded) && ctx.Err() == nil {
+				continue
+			}
+			diags.AddError(HUM_CLIENT_ERR, fmt.Sprintf("Unable to wait for deployment to complete, got error: %s", err))
+			return
+		} else if r.StatusCode() == http.StatusRequestTimeout {
+			if err := ctx.Err(); err != nil {
+				diags.AddError(HUM_CLIENT_ERR, fmt.Sprintf("Unable to wait for deployment to complete, got error: %s", err))
+				return
+			}
+			continue
+		} else if r.StatusCode() != http.StatusOK {
+			diags.AddError(HUM_API_ERR, fmt.Sprintf("Unable to wait for deployment to complete, unexpected status code: %d, body: %s", r.StatusCode(), r.Body))
+			return
+		} else {
+			data.Status = types.StringValue(string(r.JSON200.Status))
+			data.StatusMessage = types.StringValue(r.JSON200.StatusMessage)
+			data.CompletedAt = types.StringValue(r.JSON200.CompletedAt.Format(time.RFC3339))
+			if data.Status.ValueString() == "succeeded" {
+				if r, err := d.dpClient.GetDeploymentEncryptedOutputsWithResponse(ctx, d.orgId, uuid.MustParse(data.Id.ValueString())); err != nil {
+					diags.AddError(HUM_CLIENT_ERR, fmt.Sprintf("Unable to read deployment outputs, got error: %s", err))
+				} else if r.StatusCode() != http.StatusOK {
+					diags.AddError(HUM_API_ERR, fmt.Sprintf("Unable to read deployment outputs, unexpected status code: %d, body: %s", r.StatusCode(), r.Body))
+				} else {
+					if decrypted, err := age.Decrypt(base64.NewDecoder(base64.StdEncoding, strings.NewReader(r.JSON200.Raw)), outputsKey); err != nil {
+						diags.AddError(HUM_API_ERR, fmt.Sprintf("Unable to decrypt deployment outputs, got error: %s", err))
+					} else if raw, err := io.ReadAll(decrypted); err != nil {
+						diags.AddError(HUM_API_ERR, fmt.Sprintf("Unable to read deployment outputs, got error: %s", err))
+					} else {
+						data.Outputs = types.StringValue(string(raw))
+					}
+				}
+			}
+			return
+		}
+	}
+}
+
+func (d *DeploymentResource) Create(ctx context.Context, request resource.CreateRequest, response *resource.CreateResponse) {
+	var data DeploymentResourceModel
+	response.Diagnostics.Append(request.Plan.Get(ctx, &data)...)
+	if response.Diagnostics.HasError() {
+		return
+	}
+
+	outputsKey := d.doDeployment(ctx, &data, response.Diagnostics)
+	if response.Diagnostics.HasError() {
+		return
+	}
+	response.Diagnostics.Append(response.State.Set(ctx, &data)...)
+	if data.WaitFor.ValueBool() {
+		d.waitForDeployment(ctx, &data, response.Diagnostics, outputsKey)
+		response.Diagnostics.Append(response.State.Set(ctx, &data)...)
+	}
+}
+
+func (d *DeploymentResource) Read(ctx context.Context, request resource.ReadRequest, response *resource.ReadResponse) {
+	var data DeploymentResourceModel
+	response.Diagnostics.Append(request.State.Get(ctx, &data)...)
+	if response.Diagnostics.HasError() {
+		return
+	}
+
+	if r, err := d.dpClient.GetDeploymentWithResponse(ctx, d.orgId, uuid.MustParse(data.Id.ValueString())); err != nil {
+		response.Diagnostics.AddError(HUM_PROVIDER_ERR, fmt.Sprintf("Unable to read deployment, got error: %s", err))
+		return
+	} else if r.StatusCode() != http.StatusOK {
+		response.Diagnostics.AddError(HUM_API_ERR, fmt.Sprintf("Unable to read deployment, unexpected status code: %d, body: %s", r.StatusCode(), r.Body))
+		return
+	} else {
+		data.Mode = types.StringValue(r.JSON200.Mode)
+		data.RunnerId = types.StringValue(r.JSON200.RunnerId)
+		data.Status = types.StringValue(r.JSON200.Status)
+		data.StatusMessage = types.StringValue(r.JSON200.StatusMessage)
+		data.CreatedAt = types.StringValue(r.JSON200.CreatedAt.Format(time.RFC3339))
+		data.Outputs = types.StringUnknown()
+		data.WaitFor = types.BoolValue(false)
+	}
+	response.Diagnostics.Append(response.State.Set(ctx, &data)...)
+}
+
+func (d *DeploymentResource) Update(ctx context.Context, request resource.UpdateRequest, response *resource.UpdateResponse) {
+	var data DeploymentResourceModel
+	response.Diagnostics.Append(request.Plan.Get(ctx, &data)...)
+	if response.Diagnostics.HasError() {
+		return
+	}
+
+	outputsKey := d.doDeployment(ctx, &data, response.Diagnostics)
+	if response.Diagnostics.HasError() {
+		return
+	}
+	response.Diagnostics.Append(response.State.Set(ctx, &data)...)
+	if data.WaitFor.ValueBool() {
+		d.waitForDeployment(ctx, &data, response.Diagnostics, outputsKey)
+		response.Diagnostics.Append(response.State.Set(ctx, &data)...)
+	}
+}
+
+func (d *DeploymentResource) Delete(ctx context.Context, request resource.DeleteRequest, response *resource.DeleteResponse) {
+}
